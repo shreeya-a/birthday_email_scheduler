8d6b08b42e09d33e1d3d72ae5c05c2284aea3fe7
shreeya-a <shreeyadhikari11@gmail.com>
2025-02-06 16:29:38 +0545

    pandas

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-# birthday_email_automator
-
-# Birthday Email Automator
-# Objective: Send automated birthday wishes to friends and family.
-# Tasks:
-# Use a CSV file or dictionary to store names, birthdays, and email addresses.
-# Check the current date and match it to birthdays.
-# Use smtplib to send a personalized birthday wish email.
-# Automate the script to run daily with schedule.
-
+import pandas as pd
+import datetime
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-# import pandas as pd
-import csv
-from datetime import date
-
-today = date.today()
-month = today.strftime("%m")
-print(month)
+from email.mime.text import MIMEText
 
 
-
-Birthday_File = "Birthday_Tracker.csv"
-
-
-def send_birthday_email(name, email):
+def send_email(name, email):
     sender_email = "shreeyadhikari11@gmail.com"
     receiver_email = email
     password = "qurm wfoz bhse jdzb"
@@ -32,19 +13,19 @@ def send_birthday_email(name, email):
     message = MIMEMultipart()
     message["From"] = sender_email
     message["To"] = receiver_email
-    message["Subject"] = "A warm birthday wish"
+    message["Subject"] = "BIRTHDAY WISHES!"
     
     body = f"""
-    Dearest {name},
+Dearest {name},
 
-    Wishing you a very very Happy Birthday!
-    Best wishes for all your future endeavour.
-    
-    
+Wishing you a very very Happy Birthday!
 
-        """
+
+P.S This is an automated email sent using Python.
+    """
+    
     message.attach(MIMEText(body, "plain"))
-    
+
     # send the email
     try:
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
@@ -54,23 +35,29 @@ def send_birthday_email(name, email):
         print("Email sent successfully!")
     except Exception as e:
         print(f"Error: {e}")
-    
-    
-  
 
+# Load Dataframe
 try:
-    with open(Birthday_File, mode= 'r', newline="") as file:
-        reader = csv.DictReader(file)
-        for data in reader:
-            if data["BirthDay"][5:7] == month:
-                name = data["Name"]
-                email = data["Email"]
-                print(name, email)
-                send_birthday_email(name, email)
-            
-            
-        
-except Exception as e:
-    print(f"Error:{e}")
-            
+    
+    df = pd.read_csv("Birthday_Tracker.csv")
+    print(df)
+except FileNotFoundError:
+    print("Error: Birthday_Tracker.csv file not found ")
+    exit()
+    
+# Get today's date
+today = datetime.datetime.now()
+today_month = today.month
 
+# Iterate through rows in DateFrame
+for index, row in df.iterrows():
+    birthday = pd.to_datetime(row['BirthDay'])
+    birthday_month = birthday.month
+    
+    
+    if birthday_month == today_month:
+        name = row['Name']
+        email = row['Email']
+        print(f"Today is {name}'s birthday!")
+        
+        send_email(name, email)
